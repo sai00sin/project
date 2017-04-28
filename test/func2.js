619445579119f77c00aa0f60ed1481263cb20739
0004079 井上 幸信 <saishin_inoue@freesale.local>
2017-04-28 12:33:55 +0900

    new

--- test/func2.js
+++ test/func2.js
@@ -12,6 +12,42 @@ http://d.hatena.ne.jp/m-hiyama/20070208/1170905900
 
 */
 
+
+/*
+
+β変換の具体例
+
+1.まず、先頭の'λx.'は取り去って、ラムダ式（関数定義）本体を取り出します。
+2.その本体内に出現する変数xをすべて（0箇所かもしれないし、100箇所かもしれない）、引数の式で置き換えます。
+3.もちろん、変数の名前は'x'じゃなくてもいいです。
+4.λ(x, y).(…x…y…) のようなときは、変数xだけじゃなくて変数yも対応する式で置き換えます。
+
+例：
+
+(λx.x*x)（(λy.2*y)(2)）
+
+外側：
+（(λy.2*y)(2)）*（(λy.2*y)(2)）
+(2*2)*(2*2)
+
+内側：
+(λx.x*x)（(λy.2*2)(2)）
+(λx.x*x)（2*2）
+（2*2）*（2*2）
+
+8
+
+*/
+
+
+
+
+
+
+
+
+
+
 /*
 var answer (function (x, y) {
 	return f(y, x);
@@ -40,4 +76,18 @@ console.log(y)
 	}
 }());
 
-// http://d.hatena.ne.jp/m-hiyama/20070208/1170905900
+/*
+jsラムダ
+http://d.hatena.ne.jp/m-hiyama/20070208/1170905900
+
+scala関数
+https://codezine.jp/article/detail/5251
+
+http://yakinikunotare.boo.jp/orebase2/scala/function/literal
+
+val myadd:(Int, Int) => Int = (a:Int, b:Int) => {
+  a + b
+}:Int
+
+
+*/
