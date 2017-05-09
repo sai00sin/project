12ec8ab985cf5d9607e133c284f4ebc35d7175d9
0004079 井上 幸信 <saishin_inoue@freesale.local>
2017-05-09 12:35:24 +0900

    scala change

--- scala/scala_sample/src/main/scala/Main.scala
+++ scala/scala_sample/src/main/scala/Main.scala
@@ -20,5 +20,40 @@ object Main {
 	println(((x: Int, y: Int) => f(y, x))(2, 5))
 */
 
+/*
+	def f(x: Int, y: Int) = {
+		x + 3 * y
+	}
+
+	def g(x: Int) = {
+		x * x
+	}
+
+	def h(x: Int) = {
+		x * x
+	}
+
+	println(((x: Int, y: Int) => g(f(x, y)))(3, 9))
+*/
+
+	val inc: Int => Int = x => x + 1
+
+	println(inc(8))
+
   }
 }
+
+/*
+β変換の具体例
+1.まず、先頭の'λx.'は取り去って、ラムダ式（関数定義）本体を取り出します。
+2.その本体内に出現する変数xをすべて（0箇所かもしれないし、100箇所かもしれない）、引数の式で置き換えます。
+3.もちろん、変数の名前は'x'じゃなくてもいいです。
+4.λ(x, y).(…x…y…) のようなときは、変数xだけじゃなくて変数yも対応する式で置き換えます。
+*/
+
+/*
+参考サイト
+http://d.hatena.ne.jp/m-hiyama/20070208/1170905900
+
+http://qiita.com/shigemk2/items/3a5e5df9fe85cf0b4eb2
+*/
