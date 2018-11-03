1833b05bdd3bceddedeb19578b60df3275a68821
井上幸信 <saishininoue@gmail.com>
2018-11-03 13:00:50 +0900

    ios app production started

---
+++ swift/manholeSNSApp/manholeSNSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  manholeSNSApp
+//
+//  Created by USER on 2018/11/03.
+//  Copyright © 2018 Saishin Inoue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ swift/manholeSNSApp/memo.md
@@ -0,0 +1,19 @@
+single view application
+
+ユーザ登録画面
+マンホールのリストが表示される画面
+マンホールの画像を登校する画面
+マンホールの画面にコメントをつける画面
+
+画面遷移はNavigationController
+
+リストはUITableView
+
+
+
+
+
+
+
+
+
